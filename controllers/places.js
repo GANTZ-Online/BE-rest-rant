e8fa97b753d845cbd5950351fe28373c9ef50545
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-05 22:47:43 -0700

    update

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.sender('GET /places')
 })
-
+// GET /places
+app.get('/', (req, res) => {
+    res.render('places/index')
+  })
+  
 module.exports = router
