c8ab9f8a5fc9dd8f5e552fea961655c2096ea7bc
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-10 15:16:53 -0700

    checkpoint

--- controllers/places.js
+++ controllers/places.js
@@ -11,6 +11,21 @@ router.get('/new', (req, res) => {
  
 
   })
+
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
+  
   
   router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
