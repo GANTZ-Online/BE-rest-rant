19c89a8677bda7a069524c71e3e6e1f33276e3b7
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-18 13:37:06 -0700

    HW

--- controllers/places.js
+++ controllers/places.js
@@ -89,47 +89,71 @@
   
 //   module.exports = router
 
-const router = require('express').Router()
-const db = require('../models')
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
 
 router.get('/', (req, res) => {
     db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
-
-
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
-
+        .then((places) => {
+            res.render('places/index', { places })
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+        })
+});
 
 router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+    res.render('places/new')
+});
 
 router.get('/:id', (req, res) => {
+    db.Place.findById(req.params.id)
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments)
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+});
+
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+
+    db.Place.create(req.body)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+});
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
   db.Place.findById(req.params.id)
-  .populate('comments')
   .then(place => {
-      console.log(place.comments)
-      res.render('places/show', { place })
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
   })
   .catch(err => {
-      console.log('err', err)
       res.render('error404')
   })
 })
@@ -138,40 +162,19 @@ router.get('/:id', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
+    res.send('PUT /places/:id stub')
+});
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
+    res.send('DELETE /places/:id stub')
+});
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
+    res.send('GET edit form stub')
+});
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
-})
+});
 
-module.exports = router
-
-    
-
-  
+module.exports = router;
