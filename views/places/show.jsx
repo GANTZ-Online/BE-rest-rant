71ab0236e686c1a934d9a26865615c2e357b7200
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-20 21:28:18 -0700

    submission

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,41 +1,80 @@
-const React = require('react');
-const Default = require('../default');
-
-function show (data) {
+function show(data) {
   let comments = (
     <h3 className="inactive">
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '★'
+    }
+    rating = (
+      <h3>{stars} stars</h3>
+    )
+  }
+
   if (data.place.comments.length) {
     comments = data.place.comments.map(c => {
       return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
+        <div className="border col-sm-4">
+          {/* Add comment content here */}
         </div>
       )
     })
   }
+
   return (
-      <Def>
-        <main>
-          <div className="row">
+    <Def>
+      <main>
+        <div className="row">
+          ...
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            {rating}
+            <br />
+            {/* Add comment section here */}
             ...
           </div>
-          <hr />
-          <h2>Comments</h2>
-          {comments}
-        </main>
-      </Def>
+        </div>
+      </main>
+    </Def>
   )
 }
 
-module.exports = show
+function EditPlace(data) {
+  return (
+    <div>
+      <h1>Edit Place</h1>
+      <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+        ...
+        <div className="row">
+          ...
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input className="form-control"
+              id="founded"
+              name="founded"
+              value={data.place.founded}
+            />
+          </div>
+        </div>
+        ...
+        <input className="btn btn-primary" type="submit" value="Update Place" />
+      </form>
+    </div>
+  )
+}
 
 
 
