119836dc4e2c7949c716dc52d4e5105049ffc000
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-26 12:55:17 -0700

    update

--- models/index.js
+++ models/index.js
@@ -4,3 +4,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
+
+mongoose = require('mongoose') require("dotenv").config(); 
+mongoose.connect(process.env.MONGO_URI) 
+module.exports.Place = require("./places");
